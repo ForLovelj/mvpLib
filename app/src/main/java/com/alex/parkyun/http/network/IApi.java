633cd1b930a63df1abc959a28e2ad9645bc644c2
ForLovelj <624627718@qq.com>
2018-02-09 18:27:41 +0800

    Update IApi.java

--- app/src/main/java/com/alex/parkyun/http/network/IApi.java
+++ app/src/main/java/com/alex/parkyun/http/network/IApi.java
@@ -14,6 +14,15 @@ import retrofit2.http.POST;
 
 public interface IApi {
 
+    //Retrofit的Url组合规则
+      //baseUrl                             //和URL有关的注解中提供的值     //最后api的结果url
+//    http://localhost:4567/path/to/other/	/post	                    http://localhost:4567/post
+//    http://localhost:4567/path/to/other/	post	                    http://localhost:4567/path/to/other/post
+//    http://localhost:4567/path/to/other/	https://github.com/ikidou	https://github.com/ikidou
+//    如果你在注解中提供的url是完整的url，则url将作为请求的url。
+//    如果你在注解中提供的url是不完整的url，且不以 / 开头，则请求的url为baseUrl+注解中提供的值
+//    如果你在注解中提供的url是不完整的url，且以 / 开头，则请求的url为baseUrl的主机部分+注解中提供的值
+    
     /**
      * 首页数据
      * @return
